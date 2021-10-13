31458d063c5cf5275d098de284e31e48ae8aa4cc
FranRV <franratvar7@gmail.com>
2021-10-13 15:12:27 +0200

    Arrastrando imagen

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener("load",start,false);
 
-var foto,texto;
+var foto,destino,texto;
 
 function start(){
 
@@ -10,21 +10,41 @@ function start(){
 
     foto = document.getElementById("foto");
     texto = document.getElementById("texto");
+    destino = document.getElementById("destino");
 
     foto.addEventListener("dragstart",dragStart,false);  //Se desencadena cuando pinchas y COMIENZAS a mover el raton
     foto.addEventListener("drag",drag,false);  //Se desencadena durante el MOVIMIENTO del objeto
     foto.addEventListener("dragend",dragEnd,false);  //Se desencadena cuando pinchas, mueves el raton y SUELTAS
+
+    destino.addEventListener("dragenter",dragEnterDestino,false);
+    destino.addEventListener("dragover",dragOverDestino,false);
+    destino.addEventListener("drop",dropDestino,false);
 }
 
 
-function dragStart(){
+function dragStart(e){
     texto.style.color = "blue";
+    var codigo = "<img src='"+ foto.getAttribute("src")+"' width='150' height='300'>";
+    e.dataTransfer.setData("Text",codigo);
 }
 
-function drag(){
+function drag(e){
     texto.style.color = "red";
 }
 
-function dragEnd(){
+function dragEnd(e){
     texto.style.color = "green";
+}
+
+function dragEnterDestino(e){
+    e.preventDefault();
+}
+
+function dragOverDestino(e){
+    e.preventDefault();
+}
+
+function dropDestino(e){
+    e.preventDefault();
+    destino.innerHTML = e.dataTransfer.getData("Text");
 }
